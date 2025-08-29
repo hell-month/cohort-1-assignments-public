a93fe713cbea40a25ec498dd6491b6bbfa5c2809
JuyeongKim <juyeongkim@crossangle.io>
2025-08-29 12:15:05 +0900

    feat: Implement freeMintTo, freeMintToSender

--- 1a/src/MockERC20.sol
+++ 1a/src/MockERC20.sol
@@ -6,12 +6,19 @@ import {IMockERC20} from "./IMockERC20.sol";
 
 // Add as many variables or functions as you would like
 // for the implementation. The goal is to pass `forge test`.
+
 contract MockERC20 is ERC20, IMockERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
 
-    // Implement
-    function freeMintTo(uint256 amount, address to) external {}
+    // 특정 주소에 토큰 발행
+    function freeMintTo(uint256 amount, address to) external {
+        // _update(address(0), to, amount);
+        _mint(to, amount);
+    }
 
-    // Implement
-    function freeMintToSender(uint256 amount) external {}
+    // 호출자에게 토큰 발행
+    function freeMintToSender(uint256 amount) external {
+        // _update(address(0), msg.sender, amount);
+        _mint(msg.sender, amount);
+    }
 }
