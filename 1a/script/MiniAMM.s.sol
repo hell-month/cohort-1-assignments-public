7af6b520488fdc26429e1d9e603a68e88f5627f0
chosunghyun18 <krismon@naver.com>
2025-08-31 22:29:06 +0900

    build : invalid opcode: opcode 0x5e not defined - add evm_version

--- 1a/script/MiniAMM.s.sol
+++ 1a/script/MiniAMM.s.sol
@@ -15,12 +15,38 @@ contract MiniAMMScript is Script {
 
     function run() public {
         vm.startBroadcast();
-
-        // Deploy mock ERC20 tokens
-        token0 = new MockERC20("a","a");
-        token1 = new MockERC20("b","b");
-        // Deploy MiniAMM with the tokens
-        miniAMM = new MiniAMM(address(token0),address(token1));
+        MockERC20 tokenA = new MockERC20("TestDD", "tD");
+        MockERC20 tokenB = new MockERC20("TestEE", "tE");
+        MiniAMM amm = new MiniAMM(address(tokenA), address(tokenB));
         vm.stopBroadcast();
     }
 }
+
+// ==========================
+
+// ##### flare-coston2
+// ✅  [Success] Hash: 0xa732c755b1e3ff20762cd60e02c76e25648c0311f5aead00d084cc3d74b24947
+// Contract Address: 0xCbcff4Bd5127A3F1Fb5779C096DDc2130353BF68
+// Block: 21422986
+// Paid: 0.0615714375 C2FLR (985143 gas * 62.5 gwei)
+
+
+// ##### flare-coston2
+// ✅  [Success] Hash: 0xe7cef43fec2fd8345fc62ee656376c7440e418692f94f2fddcda7aeaf05e67d8
+// Contract Address: 0xFA25d25638db963ed9cB77FE7A106be5316c95bA
+// Block: 21422989
+// Paid: 0.0615714375 C2FLR (985143 gas * 62.5 gwei)
+
+
+// ##### flare-coston2
+// ✅  [Success] Hash: 0xeceabb758a179707dee7b30d5e3e5d1f61f2f30c2100b03b8af140a2abcb71db
+// Contract Address: 0x696e6580ee863398FB7DDE7B1aD8b4F89B40099F
+// Block: 21422990
+// Paid: 0.064905875 C2FLR (1038494 gas * 62.5 gwei)
+
+// ✅ Sequence #1 on flare-coston2 | Total Paid: 0.18804875 C2FLR (3008780 gas * avg 62.5 gwei)
+                                                                                                                                                                                                                
+
+// ==========================
+
+// ONCHAIN EXECUTION COMPLETE & SUCCESSFUL.
